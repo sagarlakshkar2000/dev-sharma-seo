9b6f2ccad0585cadeb01058142835615de21fc14
sagardev-office <sagarlakshkar23@gmail.com>
2026-02-23 12:47:37 +0530

    feat: Add initial WordPress `wp-config.php` with database and security settings.

--- blog/wp-config.php
+++ blog/wp-config.php
@@ -21,13 +21,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'u334046449_dev');
+define('DB_NAME', 'dev-sharma-seo');
 
 /** Database username */
-define('DB_USER', 'u334046449_dev');
+define('DB_USER', 'root');
 
 /** Database password */
-define('DB_PASSWORD', '@oa42+xGMpX1');
+define('DB_PASSWORD', '');
 
 /** Database hostname */
 define('DB_HOST', 'localhost');
